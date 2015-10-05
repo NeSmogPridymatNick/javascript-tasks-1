b229a29b208a2982cb18e43eed7f25033786d4f9
NeSmogPridymatNick <IceDruid@yandex.ru>
2015-10-05 19:36:34 +0300

    Add solution

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,26 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
-
-// Немного замечательного кода и магии
+var a = [1, 4, 5, 9, 10, 40, 50];
+var r = ['I','IV','V','IX','X','XL', 'L'];
+function aToR(number)
+{
+    var ret = '';
+    var i = a.length - 1;
+    while(number > 0)
+    {
+        if(number >= a[i])
+        {
+            ret += r[i];
+            number -= a[i];
+        }
+        else
+            i--;
+    }
+    return ret;
+}
+if ((hours<24)&&(minutes<=60)) 
+    {
+        console.log(aToR(hours),':',aToR(minutes));
+    }
+    else
+        console.log('Время указано не верно');
